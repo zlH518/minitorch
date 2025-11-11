b8bced806522d536480bf3dfc996ad1b3b8b5862
zlHuang <108509803+zlH518@users.noreply.github.com>
2025-11-12 00:34:23 +0800

    feat: 1.1, 1.2, 1.3, 1.4 (#2)

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -23,7 +23,9 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
     # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+    left = [v if i != arg else v - epsilon for i, v in enumerate(vals)]
+    right = [v if i != arg else v + epsilon for i, v in enumerate(vals)]
+    return (f(*right) - f(*left)) / (2 * epsilon)
 
 
 variable_count = 1
